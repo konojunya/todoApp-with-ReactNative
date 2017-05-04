ce807c87e8f1ff2091562eb73470b616b73ad3e7
konojunya <junya03dance@gmail.com>
2017-05-04 20:21:00 +0900

    inputとlistviewの基礎できた

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,55 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry,View, Text, TextInput, ListView, ScrollView, TouchableHighlight } from 'react-native';
+
+var array = [];
+const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 
 export default class todoApp extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+      dataSource: ds.cloneWithRows([])
+    };
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View style={{marginTop: 20}}>
+        <TextInput
+          style={{height: 40, padding: 10}}
+          placeholder="Type here to translate!"
+          onChangeText={this._onChangeText}
+          onEndEditing={this._onEndEditing}
+          value={this.state.text}
+        />
+        <ScrollView style={{marginTop: 20}}>
+          <ListView
+            dataSource={this.state.dataSource}
+            renderRow={(rowData) => <Text style={{fontSize: 20}}>{rowData}</Text>}
+          />
+        </ScrollView>
       </View>
     );
   }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  _onChangeText = (value) => {
+    this.setState({
+      text: value
+    })
+  }
+
+  _onEndEditing = () => {
+
+    array.push(this.state.text)
+
+    this.setState({
+      text: "",
+      dataSource: ds.cloneWithRows(array)
+    })
+  }
+
+}
 
 AppRegistry.registerComponent('todoApp', () => todoApp);
